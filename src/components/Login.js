4b50f08335785427296c9b426edccf9b21b11952
Lemmy Mwaura <lemminton@gmail.com>
2022-05-31 17:39:22 +0300

    feat: login component and user reducer updated

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react"
+import { useDispatch } from "react-redux";
+import { login, logout } from "../features/user";
 
 export default function Login() {
+  const dispatch = useDispatch()
+
   return (
     <div>
-      <button>Login</button>
+      <button
+        onClick={() =>
+          dispatch(
+            login({
+              name: "LemmyMwaura",
+              age: 25,
+              email: "lemminton@gmail.com",
+            })
+          )
+        }
+      >
+        Login
+      </button>
+      <button onClick={() => dispatch(logout())}>
+        Logout
+      </button>
     </div>
   )
 }
